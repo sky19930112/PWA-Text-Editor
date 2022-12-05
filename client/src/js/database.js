1a3bf319ee4bda76fbbe388090611dc88c2b836b
sky19930112 <107167603+sky19930112@users.noreply.github.com>
2022-12-05 12:39:00 -0600

    work on database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,32 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// pretty much boilerplate from class activity
+export const putDb = async (content) => {
+  const textEditDb = await openDB('jate', 1);
+  const tx = textEditDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ jate: content });
+  const result = await request;
+
+  console.log('saved to the database', result);
+};
+
+
+
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// pretty much boilerplate from class activity
+export const getDb = async () => {
+  const textEditDb = await openDB('jate', 1);
+  const tx = textEditDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
